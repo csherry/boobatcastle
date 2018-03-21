0656e60d95e09f2ca280366c70c488ef77a2e816
Sherry Chen <sherrychen@10.0.0.18>
2018-03-20 21:04:11 -0700

    add

--- src/comp/Quiz.js
+++ src/comp/Quiz.js
@@ -17,7 +17,7 @@ class Quiz extends Component {
     }
     
     componentDidMount(){
-        this.socket = mySocket('http://localhost:10025');
+        this.socket = mySocket('https://castlequizsocket.herokuapp.com/');
         
         this.socket.on("newq", (data)=>{
             this.setState({
