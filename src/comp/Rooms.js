2c5369b55b150ef9816ad90156e533dc954263ec
Sherry Chen <sherrychen@Sherrys-MacBook-Pro.local>
2018-03-22 14:49:03 -0700

    add

--- src/comp/Rooms.js
+++ src/comp/Rooms.js
@@ -24,13 +24,14 @@ class Rooms extends Component {
             comp = (
                 <App />
             )     
-        } else if (this.state.mode === 0){
+        } else {
             <div>
                 <button onClick={this.props.handDisplay.bind(this, "room1")}>Sticker Room 1</button>
                 <button onClick={this.props.handDisplay.bind(this, "room2")}>Sticker Room 2</button>
                 <button onClick={this.props.handDisplay.bind(this, "room3")}>Sticker Room 3</button>
                 <button onClick={this.props.handDisplay.bind(this, "room4")}>Sticker Room 4</button>
                 <button onClick={this.props.handDisplay.bind(this, "room5")}>Sticker Room 5</button>  
+                <button onClick={this.leaveRoom}>Leave</button>
             </div>
         }
             
